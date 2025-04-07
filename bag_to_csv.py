e09a852bd92b7937e62af59b98c0ae2d0af5b270
mojtaba1989 <mbahramg@mtu.edu>
2025-04-07 16:57:01 -0400

    to work on

--- bag_to_csv.py
+++ bag_to_csv.py
@@ -6,6 +6,8 @@ import numpy as np
 import rospy
 import sys
 import argparse
+import multiprocessing
+from functools import partial
 
 file_path = os.path.abspath(__file__)
 dir_path = os.path.dirname(file_path)
@@ -112,11 +114,93 @@ def gen_csv(BAG_DIR, config_file=default_config_path, overwrite='Always'):
                         f.write('\n')
 
 
+def gen_csv_task(task, lock):
+    bag_name, bag_dir,  target_dir, config, config_key, overwrite, worker = task
+    
+
+    FILE_NAME = os.path.join(target_dir, '.'.join([bag_name, config_key]))
+    BAG_NAME = os.path.join(bag_dir, bag_name)
+
+    with lock:
+        print('Start processing:({},{},{})'.format(bag_name, config_key, worker), end='\n')
+
+    if os.path.exists(FILE_NAME):
+        if overwrite == 'Always':
+            pass
+        elif overwrite == 'Never':
+            return
+        elif overwrite == 'Ask':
+            print('This option is not available in multiprocessing mode!')
+            print('Skip topic: {}'.format(config_key))
+            return
+    with open(FILE_NAME, 'w') as f, rosbag.Bag(BAG_NAME) as bag:
+        f.write('time,')
+        f.write(','.join(config['cols'])+'\n')
+        if "arrays" in config.keys():
+            for topic, msg, t in bag.read_messages(topics=[config['topic']]):
+                for attr in get_nested_attr(msg, config["arrays"]["field"]) or []:
+                    row = [get_nested_attr(attr, fld) for fld in config["fields"]]
+                    row.insert(0, t)
+                    f.write(','.join(str(i) for i in row))
+                    f.write('\n')
+        else:
+            for topic, msg, t in bag.read_messages(topics=[config['topic']]):
+                row = [get_nested_attr(msg, fld) for fld in config["fields"]]
+                f.write('{},'.format(t))
+                f.write(','.join(str(i) for i in row))
+                f.write('\n')
+
+    with lock:
+        print('Finish processing:({},{},{})'.format(bag_name, config_key, worker), end='\n')
+
+        
+    return
+
+def gen_csv_multiprocessing(BAG_DIR, config_file=default_config_path, overwrite='Always'):
+    TARGET_DIR = os.path.join(BAG_DIR, 'csv')
+    print('Using bag folder: {}'.format(BAG_DIR))
+    print('Using target directory: {}'.format(TARGET_DIR))
+
+    config = check_config(config_file)
+    if config is None:
+        print('Configuration file is invalid!')
+        exit(4)
+    print('Using config: {}'.format(config_file))
+
+    print('Selected overwrite option: {}'.format(overwrite))
+    print('Number of cores: {}'.format(multiprocessing.cpu_count()))
+    print('Use -h for help')
+    print('Start converting rosbags to csv...')
+
+    bag_lst = [bag for bag in os.listdir(BAG_DIR) if bag.endswith('.bag')]
+    tasks = []
+    for bag_name in bag_lst:
+        for config_key in config.keys():
+            tasks.append((bag_name, BAG_DIR, TARGET_DIR, config[config_key], config_key, overwrite, None))
+
+    lock = multiprocessing.Manager().Lock()
+    worker_fn = partial(gen_csv_task, lock=lock)
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        pool.map(worker_fn, tasks)
+        pool.close()
+        pool.join()
+    print('Finish converting rosbags to csv...')
+    return
+    
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert rosbag to csv')
     parser.add_argument('dir', type=str, help='Directory of rosbag')
     parser.add_argument('--config_file', '-c', type=str, default=default_config_path, help='Config file')
     parser.add_argument('--overwrite', '-o', type=str, default='Never', choices=['Always', 'Never', 'Ask'], help='Overwrite csv file')
+    parser.add_argument('--multi', '-m', action='store_true', help='Use multiprocessing mode')
 
     args = parser.parse_args()
-    gen_csv(args.dir, args.config_file, args.overwrite)
+    if args.multi:
+        gen_csv_multiprocessing(args.dir, args.config_file, args.overwrite)
+        exit(0)
+    else:
+        gen_csv(args.dir, args.config_file, args.overwrite)
